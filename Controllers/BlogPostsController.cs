ae8634bb825158d53576606458ff6475f99eadb2
Gelson Hernandez <gelsonpf49@hotmail.com>
2023-10-25 14:41:35 -0400

    Removed Code

--- Controllers/BlogPostsController.cs
+++ Controllers/BlogPostsController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DevDiary.Data;
 using DevDiary.Models;
 using DevDiary.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.VisualStudio.TextTemplating;
 using System.Security.Claims;
 using X.PagedList;
 using DevDiary.Helpers;
