b5e5d15786a45c97020900066157a6259f13b5ed
Gelson Hernandez <gelsonpf49@hotmail.com>
2023-10-26 00:38:27 -0400

    Update-database

---
+++ Data/Migrations/20231026043748_003_Update.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevDiary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _003_Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
