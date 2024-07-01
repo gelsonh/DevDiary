0b8afaee819f618a844190407f2e50648d87dc5a
Gelson Hernandez <gelsonpf49@hotmail.com>
2024-07-01 13:22:15 -0400

    Added Comment in API/BlogPostController

--- Controllers/API/BlogPostsController.cs
+++ Controllers/API/BlogPostsController.cs
@@ -1,54 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using DevDiary.Data;
 using DevDiary.Models;
 using DevDiary.Services.Interfaces;
-using DevDiary.Services;
 
 namespace DevDiary.Controllers.API
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/[controller]")] // Setting the base route for this controller
+    [ApiController] // Indicating tha this is an APi controller
     public class BlogPostsController : ControllerBase
     {
         
         private readonly IBlogService _blogService;
 
-
         public BlogPostsController(ApplicationDbContext context, IBlogService blogService)
         {        
-            _blogService = blogService;
+            _blogService = blogService; // Initializing the blog service field
         }
 
-
-        /// <summary>
-        /// This endpoint will return the most recently published blog posts.
-        /// The count parameter indicates the number of recent posts to return.
-        /// The maximun number of blog posts allowed per-request is 10.
-        /// </summary>
-        /// <param name="count">The number of blog posts to retrieve</param>
-        /// <returns></returns>
-        /// 
-
-
-        [HttpGet("{count:int}")]
-        public async Task<ActionResult<IEnumerable<BlogPost>>> GetBlogPosts(int count)
+        [HttpGet("{count:int}")] // Difining a GET endpoint that takes an integer count as a parameter
+        public async Task<ActionResult<IEnumerable<BlogPost>>> GetBlogPosts(int count) // Method to get a list of blog posts
         {
 
-            if (count > 10)
+            if (count > 10) // If the count exceeds 10
             {
-                count = 10;
+                count = 10; // Limit the count to 10
             }
 
-            IEnumerable<BlogPost> blogPosts = (await _blogService.GetBlogPostAsync()).Take(count);
+            IEnumerable<BlogPost> blogPosts = (await _blogService.GetBlogPostAsync()).Take(count); // Retrieve the blog posts and take the specified number 
 
-            return Ok(blogPosts);
+            return Ok(blogPosts); // Return the blog posts with a 200 OK status
         }
 
     }
 }
+
+
